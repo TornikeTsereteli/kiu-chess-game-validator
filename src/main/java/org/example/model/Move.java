c4f1eb2ed4f15d80e3f305131908c60d47cf2f3c
tornike tsereteli <anonymous@example.com>
2025-04-18 10:14:59 +0400

    add move class, which can be used to undo the game it will be helpful for impleneting checkmate detector

---
+++ src/main/java/org/example/model/Move.java
@@ -0,0 +1,44 @@
+package org.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.example.model.enums.PieceColor;
+
+@Getter
+@Setter
+public class Move {
+
+    private Piece movedPiece;
+    private Square from;
+    private Square to;
+    private Piece capturePiece;
+
+    public Move(Piece movedPiece, Square from, Square to) {
+        this.movedPiece = movedPiece;
+        this.from = from;
+        this.to = to;
+    }
+
+    public void undo(Board board) {
+        from.setOccupyingPiece(movedPiece);
+        to.setOccupyingPiece(capturePiece);
+        movedPiece.setPosition(from);
+        if (capturePiece != null) {
+            capturePiece.setPosition(to);
+            if (capturePiece.getColor() == PieceColor.WHITE) {
+                board.getWhitePieces().add(capturePiece);
+            } else {
+                board.getBlackPieces().add(capturePiece);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Move{" +
+                "movedPiece=" + movedPiece.getClass().toString() +
+                ", from=" + from.getPosition().toAlgebraic() +
+                ", to=" + to.getPosition().toAlgebraic() +
+                '}';
+    }
+}
